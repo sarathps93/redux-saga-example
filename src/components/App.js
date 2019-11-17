72a9bc70ad34211621ead64d183ff0abe2cdd301
Sarath <sarathps93@gmail.com>
2019-11-17 19:36:46 +0000

    feat(*): babel and css setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
   return (
     <>
-    <h1>Hello</h1>
-    <div>App Home page</div>
+    <h1>Welcome to redux saga playground</h1>
+    <div>
+      <div>Section1</div>
+      <div>Section2</div>
+    </div>
     </>
   )
 };
